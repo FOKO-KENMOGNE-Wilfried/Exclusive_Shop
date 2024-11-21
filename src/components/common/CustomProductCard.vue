a52e2c8595d9c5864fe258e0b0a40d202f568e41
FOKO KENMOGNE Wilfried <darkwall@kali>
2024-11-21 05:34:01 +0100

    The today's section is almost ready and the category section too

---
+++ src/components/common/CustomProductCard.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        Product Card
+    </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
--- src/components/pages/HomePage.vue
+++ src/components/pages/HomePage.vue
@@ -4,13 +4,14 @@
             <div class="flex flex-col w-2/12 border-r pt-8">
                 <div v-for="category in categoriesList" :key="category.id" class="flex items-center justify-between cursor-pointer hover:bg-gray-200 h-10 transition-all duration-200 ease-in-out px-2">
                     <p>{{ category.label }}</p>
-                    <!-- <img src="/public/icons/icons arrow-right.svg" alt="" /> -->
+                    <!-- <img src="/public/icons/DropDown.svg" alt="dropdown" /> -->
                 </div>
             </div>
             <div class="bg-gray-400 w-full mt-8 ml-8"></div>
         </div>
         <div class="flex flex-col gap-4">
             <CustomSectionTitle label="Today's" />
+            <!--  -->
             <div class="flex items-center gap-16 text-3xl font-semibold">
                 <p>Flash Sales</p>
                 <div class="flex gap-4 items-center">
@@ -35,8 +36,44 @@
                     </div>
                 </div>
             </div>
+            <!--  -->
+            <Carousel v-bind="config" class="">
+                <Slide v-for="slide in 10" :key="slide" class="h-96 bg-gray-400">
+                    <div class="text-white">
+                        {{ slide }}
+                    </div>
+                </Slide>
+
+                <template #addons>
+                    <Navigation class="bg-black"/>
+                    <Pagination />
+                </template>
+            </Carousel>
+            <div class="flex justify-center pt-4">
+                <div class="w-48">
+                    <CustomButton :handle-click="() => null">
+                        <p>View All Products</p>
+                    </CustomButton>
+                </div>
+            </div>
         </div>
-        <div>3</div>
+        <div class="flex flex-col gap-8">
+            <CustomSectionTitle label="Categories" />
+            <h2 class="titleStyle">Browe By Catgory</h2>
+         <!--  -->
+            <Carousel v-bind="config" class="">
+                <Slide v-for="slide in 10" :key="slide" class="h-48 !w-48 bg-gray-400">
+                    <div class="text-white">
+                            {{ slide }}
+                    </div>
+                </Slide>
+
+                <template #addons>
+                    <Navigation class="bg-black"/>
+                    <Pagination />
+                </template>
+            </Carousel>
+        </div>
         <div>4</div>
         <div>5</div>
         <div>6</div>
@@ -47,8 +84,14 @@
 
 <script setup lang="ts">
 import CustomSectionTitle from '../common/CustomSectionTitle.vue';
+import "vue3-carousel/dist/carousel.css";
+import { Carousel, Slide, Pagination } from "vue3-carousel";
+import CustomButton from '../common/CustomButton.vue';
 
-
+const config = {
+  itemsToShow: 4,
+  gap: 16
+}
 const categoriesList = [
     {
         id: 1,
@@ -89,4 +132,8 @@ const categoriesList = [
 ]
 </script>
 
-<style scoped></style>
+<style scoped>
+.titleStyle {
+    @apply text-3xl font-semibold;
+}
+</style>
